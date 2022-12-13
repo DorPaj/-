8b3c20627ad9ace7b1a3da2efbfad86876d7a41a
DorPaj <100585183+DorPaj@users.noreply.github.com>
2022-12-13 21:02:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const kolt = prompt("Podaj kolor tła (biały - b, żółty - z, czarny - c)")
+document.write("<div class=" + kolt + ">")
+
+const kolp = prompt("Podaj kolor penisa (biały - b, żółty - z, czarny - c)")
+
+const dl = Number(prompt("Podaj długość kutasa"))
+document.write("<p class=" + kolp + ">8")
+for (var i = 0; i < dl; i++) {
+    document.write("=")
+}
+document.write("D</p></div>")
